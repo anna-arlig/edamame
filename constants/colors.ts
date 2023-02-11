ccfaea0c4a9dd9445fab5f7d28782789221b0662
anna-arlig <anna.arlig@iths.se>
2023-02-11 15:55:44 +0100

    Add colors, fonts and styled text components

---
+++ constants/colors.ts
@@ -0,0 +1,9 @@
+export default {
+
+    primary: {
+      background_beige: '#FFFCF9',
+      light_green: '#C3E9D2',
+      dark_green: '#214C41',
+      white: '#FFFFFF'
+    },
+    }
---
+++ constants/typography.ts
@@ -0,0 +1,43 @@
+import { Text } from 'react-native'
+import styled from 'styled-components/native'
+import colors from './colors'
+
+export const HeaderText = styled(Text)`
+  font-size: 24px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+  `
+
+export const Headline = styled(Text)`
+  font-size: 18px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+  `
+
+export const Body = styled(Text)`
+  font-size: 14px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+  `
+
+export const RecipeCategoryTitle = styled(Text)`
+  font-size: 12px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+  text-transform: uppercase;
+  `
+
+export const ChipTitle = styled(Text)`
+  font-size: 12px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+  text-transform: uppercase;
+`
+
+export const RecipeCardTitle = styled(Text)`
+  font-size: 12px;
+  font-family: 'Quicksand-Regular';
+  color: ${colors.primary.dark_green};
+`
+
+
