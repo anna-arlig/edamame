ce5e3496b44dbf72f04f84dab53754b2a0a38fab
anna-arlig <anna.arlig@iths.se>
2023-02-12 11:27:24 +0100

    Fix font error and add styled components types to tsconfig

--- constants/typography.ts
+++ constants/typography.ts
@@ -4,39 +4,39 @@ import colors from './colors'
 
 export const HeaderText = styled(Text)`
   font-size: 24px;
-  font-family: 'Quicksand-Light';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
   `
 
 export const Headline = styled(Text)`
   font-size: 18px;
-  font-family: 'Quicksand-Regular';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
   `
 
 export const Body = styled(Text)`
   font-size: 14px;
-  font-family: 'Quicksand-Regular';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
   `
 
 export const RecipeCategoryTitle = styled(Text)`
   font-size: 12px;
-  font-family: 'Quicksand-Regular';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
   text-transform: uppercase;
   `
 
 export const ChipTitle = styled(Text)`
   font-size: 12px;
-  font-family: 'Quicksand-Regular';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
   text-transform: uppercase;
 `
 
 export const RecipeCardTitle = styled(Text)`
   font-size: 12px;
-  font-family: 'Quicksand-Regular';
+  font-family: 'Quicksand';
   color: ${colors.primary.dark_green};
 `
 
